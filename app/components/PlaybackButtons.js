07bf342a1451e61e4c3ed3c655a61568a7ce90a6
si3792 <simo.t.iliev@industria.tech>
2017-09-04 12:59:18 +0300

    Added click logic and more styling for playback buttons

--- app/components/PlaybackButtons.js
+++ app/components/PlaybackButtons.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const PlaybackButtons = (playback) => (
-  <div className="playback-buttons">
-    <button><span className="glyphicon glyphicon-play" aria-hidden="true" /> </button>
-    <button><span className="glyphicon glyphicon-pause" aria-hidden="true" /> </button>
-    <button><span className="glyphicon glyphicon-stop" aria-hidden="true" /> </button>
-  </div>
-);
+class PlaybackButtons extends Component {
 
+  props: {
+    onClick: () => void,
+    playback: string
+  }
+
+  handleClick = (e) => {
+    this.props.onClick(e.currentTarget.value);
+  }
+
+  render() {
+    return (
+      <div className="playback-buttons">
+        <button className={this.props.playback === 'PLAYING' ? 'active' : ''} value="play" onClick={this.handleClick}>
+          <span className="glyphicon glyphicon-play" aria-hidden="true" />
+        </button>
+        <button className={this.props.playback === 'PAUSED' ? 'active' : ''} value="pause" onClick={this.handleClick}>
+          <span className="glyphicon glyphicon-pause" aria-hidden="true" />
+        </button>
+        <button className={this.props.playback === 'STOPPED' ? 'active' : ''} value="stop" onClick={this.handleClick}>
+          <span className="glyphicon glyphicon-stop" aria-hidden="true" />
+        </button>
+      </div>
+    );
+  }
+}
 export default PlaybackButtons;
--- app/containers/YoutubePage.js
+++ app/containers/YoutubePage.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PlaybackButtons from '../components/PlaybackButtons';
+import { playVideo, pauseVideo, stopVideo } from '../actions/playback';
 
-export default class YoutubePage extends Component {
+
+class YoutubePage extends Component {
+
+  props: {
+    sendAction: () => void,
+    playback: string
+  }
+
+  handlePlaybackButtonsClick = (val) => {
+    if (val === 'play') this.props.sendAction(playVideo());
+    else if (val === 'pause') this.props.sendAction(pauseVideo());
+    else if (val === 'stop') this.props.sendAction(stopVideo());
+  }
+
   render() {
     return (
       <div>
         <h1>Youtube page</h1>
-        <PlaybackButtons />
+        <PlaybackButtons onClick={this.handlePlaybackButtonsClick} playback={this.props.playback} />
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  playback: state.playback
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  sendAction: (action) => { dispatch(action); }
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(YoutubePage);
