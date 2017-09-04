cd6bf557ec45af2bf300c12e7585c5d20fde3ad0
si3792 <simo.t.iliev@industria.tech>
2017-09-04 11:16:24 +0300

    Added css reset and sass main file

--- app/index.js
+++ app/index.js
@@ -3,7 +3,8 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
-import './app.global.css';
+import './css-reset.global.css';
+import './app.global.scss';
 
 const store = configureStore();
 
